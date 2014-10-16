d0936f0b81d916d47493a1a6059ef8dba6374dd3
Alvaro Aragoneses bl0800 <aj.aragoneses@gmail.com>
2014-10-16 12:57:58 +0200

    [Test] Ampliado TestCoche para contemplar las 3 categorias de coches

--- src/test/java/es/upm/miw/pd/doo/alquilerTest/TestCoche.java
+++ src/test/java/es/upm/miw/pd/doo/alquilerTest/TestCoche.java
@@ -10,32 +10,44 @@ import es.upm.miw.pd.doo.alquiler.Coche;
 
 public class TestCoche {
 
-	private Coche c;
+	private Coche c1;
+	private Coche c2;
+	private Coche c3;
 	
 	@Before
 	public void init(){
-		this.c = new Coche("Seat Ibiza", Categoria.C);
+		this.c1 = new Coche("Seat Ibiza", Categoria.A);
+		this.c2 = new Coche("Mazda V6", Categoria.B);
+		this.c3 = new Coche("Lamborghini Diablo", Categoria.C);
 	}
 	
 	
 	@Test
 	public void testDescripcionCoche(){
-		assertEquals("(Coche:Seat Ibiza)", c.toString());
+		assertEquals("(Coche:Seat Ibiza)", c1.toString());
+		assertEquals("(Coche:Mazda V6)", c2.toString());
+		assertEquals("(Coche:Lamborghini Diablo)", c3.toString());
 	}
 	
 	@Test
 	public void testAlquilerCoche1dia(){
-		assertEquals(20, c.obtenerPrecioAlquiler(1));
+		assertEquals(10, c1.obtenerPrecioAlquiler(1));
+		assertEquals(15, c2.obtenerPrecioAlquiler(1));
+		assertEquals(20, c3.obtenerPrecioAlquiler(1));
 	}
 	
 	@Test
 	public void testAlquilerCoche5dias(){
-		assertEquals(92, c.obtenerPrecioAlquiler(5));
+		assertEquals(46, c1.obtenerPrecioAlquiler(5));
+		assertEquals(67, c2.obtenerPrecioAlquiler(5));
+		assertEquals(92, c3.obtenerPrecioAlquiler(5));
 	}
 	
 	
 	@Test
 	public void testAlquilerCoche14dias(){
-		assertEquals(194, c.obtenerPrecioAlquiler(14));
+		assertEquals(97, c1.obtenerPrecioAlquiler(14));
+		assertEquals(163, c2.obtenerPrecioAlquiler(14));
+		assertEquals(194, c3.obtenerPrecioAlquiler(14));
 	}
 }
