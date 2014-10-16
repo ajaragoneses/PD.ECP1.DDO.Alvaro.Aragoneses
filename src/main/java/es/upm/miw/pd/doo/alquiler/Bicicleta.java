23b2a4e0f40b5d4d9827802ffb77f2a539362d46
Alvaro Aragoneses bl0800 <aj.aragoneses@gmail.com>
2014-10-16 12:18:40 +0200

    [VistaPublica] Creadas las clases Coche, Moto y Bicicleta

---
+++ src/main/java/es/upm/miw/pd/doo/alquiler/Bicicleta.java
@@ -0,0 +1,11 @@
+package es.upm.miw.pd.doo.alquiler;
+
+public class Bicicleta extends Vehiculo {
+
+	@Override
+	public int obtenerPrecioAlquiler(int dias) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/es/upm/miw/pd/doo/alquiler/Coche.java
@@ -0,0 +1,11 @@
+package es.upm.miw.pd.doo.alquiler;
+
+public class Coche extends Vehiculo {
+
+	@Override
+	public int obtenerPrecioAlquiler(int dias) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
