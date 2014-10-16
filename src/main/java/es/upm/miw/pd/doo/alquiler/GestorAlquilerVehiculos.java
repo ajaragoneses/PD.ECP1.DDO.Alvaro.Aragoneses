b0fb4cf3eeea61a44f82a95f950a0e0ff42b2b3a
Alvaro Aragoneses bl0800 <aj.aragoneses@gmail.com>
2014-10-16 12:22:27 +0200

    [VistaPublica] Creada la clase GestorAlquilerVehiculos y sus métodos publicos

---
+++ src/main/java/es/upm/miw/pd/doo/alquiler/GestorAlquilerVehiculos.java
@@ -0,0 +1,22 @@
+package es.upm.miw.pd.doo.alquiler;
+
+import java.util.ArrayList;
+
+public class GestorAlquilerVehiculos {
+
+	private ArrayList listaVehiculos = new ArrayList();
+	
+	public void altaVehiculo(){
+		// TODO...
+	}
+	
+	public String verListaVehiculos(){
+		// TODO...
+		return null;
+	}
+	
+	public int calcularPrecio(int idVehiculo, int dias){
+		// TODO...
+		return 0;
+	}
+}
