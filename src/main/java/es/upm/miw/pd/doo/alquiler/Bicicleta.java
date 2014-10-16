a2a252e782e5d02f92ddece0719f6a9f54bb63a8
Alvaro Aragoneses bl0800 <aj.aragoneses@gmail.com>
2014-10-16 13:11:01 +0200

    [Test] Añadida la clase TestBicicleta y modificada la interfaz de la clase Bicicleta

--- src/main/java/es/upm/miw/pd/doo/alquiler/Bicicleta.java
+++ src/main/java/es/upm/miw/pd/doo/alquiler/Bicicleta.java
@@ -2,6 +2,10 @@ package es.upm.miw.pd.doo.alquiler;
 
 public class Bicicleta extends Vehiculo {
 
+	public Bicicleta(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public int obtenerPrecioAlquiler(int dias) {
 		// TODO Auto-generated method stub
---
+++ src/test/java/es/upm/miw/pd/doo/alquilerTest/TestBicicleta.java
@@ -0,0 +1,41 @@
+package es.upm.miw.pd.doo.alquilerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.upm.miw.pd.doo.alquiler.Bicicleta;
+
+public class TestBicicleta {
+
+	private Bicicleta b;
+	
+	@Before
+	public void init(){
+		this.b = new Bicicleta("BMX KHE Evo 0.3");
+	}
+	
+	
+	@Test
+	public void testDescripcionBicicleta(){
+		assertEquals("(Bicicleta:BMX KHE Evo 0.3)", b.toString());
+	}
+	
+	@Test
+	public void testAlquilerBicicleta1dia(){
+		assertEquals(3, b.obtenerPrecioAlquiler(1));
+	}
+	
+	@Test
+	public void testAlquilerBicicleta5dias(){
+		assertEquals(12, b.obtenerPrecioAlquiler(5));
+	}
+	
+	
+	@Test
+	public void testAlquilerBicicleta14dias(){
+		assertEquals(30, b.obtenerPrecioAlquiler(14));
+	}
+
+}
